1033253fadbe88e832d5d11af4dabbbe9a1730e0
Артём Упоров <aruporoff@yandex.ru>
2025-03-27 16:17:43 +0500

    Добавили комментарии

--- ApplicationContext.cs
+++ ApplicationContext.cs
@@ -2,13 +2,17 @@
 
 namespace LB1
 {
+    //Контекст предназначен для подключения к базе данных
     internal class ApplicationContext: DbContext
     {
+        //Свойство Users позволяет контексту работать с таблицей Users
         public  DbSet<User> Users { get; set; } = null;
         public ApplicationContext()
         {
+            //Создаём базу данных и таблицы в ней
             Database.EnsureCreated();
         }
+        //OnConfiguring этот метод содержит настройки для подключения к БД
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb2;Username=postgres;Password=1111");
--- Program.cs
+++ Program.cs
@@ -7,13 +7,14 @@ namespace LB1
         {
             using (ApplicationContext db = new ApplicationContext())
             {
+                //объявлем и создаём объекты user1 и user2, иницилизируем в эти переменные данные
                 User user1 = new User { Name = "Том", Age = 33 };
                 User user2 = new User { Name = "Алиса", Age = 26 };
-
+                //Обращаемся к таблице и сохраняем БД
                 db.Users.AddRange(user1, user2);
                 db.SaveChanges();
             }
-
+            //инцилизируем конфигуратор приложений
             ApplicationConfiguration.Initialize();
             Application.Run(new FormMain());
         }
